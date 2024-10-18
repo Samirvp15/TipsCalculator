299e47125c3a79bc269f35572b4ea4cfeafaeb5f
Samir Vergara Pastor <132174621+Samirvp15@users.noreply.github.com>
2024-10-18 16:16:59 -0500

    add initProject with Tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+function App() {
+  
+
+  return (
+    <>
+
+
+    <header className=" bg-lime-500 py-5">
+      <h1 className="">Calculador de Propinas y Consumo</h1>
+    </header>
+     
+
+
+    </>
+  )
+}
+
+export default App
