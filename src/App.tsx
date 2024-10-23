b5f5230013b297c31ba2a422412a60bda1a5bbad
Samir Vergara Pastor <132174621+Samirvp15@users.noreply.github.com>
2024-10-22 23:55:16 -0500

    add component OrderTotal and subTotal funct

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import MenuItem from "./components/MenuItem"
 import OrderContents from "./components/OrderContents"
+import OrderTotals from "./components/OrderTotals"
 import { menuItems } from "./data/db"
 import UseOrder from "./hooks/useOrder"
 
@@ -7,43 +8,48 @@ import UseOrder from "./hooks/useOrder"
 
 
 function App() {
-  
-  const {order, addItem, removeItem} = UseOrder()
+
+  const { order, addItem, removeItem } = UseOrder()
 
   return (
     <>
 
 
-    <header className=" bg-lime-500 py-5">
-      <h1 className=" text-center text-4xl font-black">Calculador de Propinas y Consumo</h1>
-    </header>
-     
-    <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2"> 
-      <div className=" p-5">
-      <h2 className=" text-4xl font-black">Menu</h2>
+      <header className=" bg-lime-500 py-5">
+        <h1 className=" text-center text-4xl font-black">Calculador de Propinas y Consumo</h1>
+      </header>
 
-        <div className=" space-y-3 mt-10">
-          {menuItems.map(item=>(
-            <MenuItem
-              key={item.id}
-              item={item}
-              addItem={addItem}
-            />
-          ))}
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div className=" p-5">
+          <h2 className=" text-4xl font-black">Menu</h2>
+
+          <div className=" space-y-3 mt-10">
+            {menuItems.map(item => (
+              <MenuItem
+                key={item.id}
+                item={item}
+                addItem={addItem}
+              />
+            ))}
+          </div>
         </div>
-      </div>
 
-      <div className=" border border-dashed border-slate-500 p-5 rounded-lg space-y-10">
+        <div className=" border border-dashed border-slate-500 p-5 rounded-lg space-y-10">
           <OrderContents
-          
+
             order={order}
             removeItem={removeItem}
           />
-        
-      </div>
+
+          <OrderTotals
+
+            order={order}
+          />
+
+        </div>
 
 
-    </main>
+      </main>
 
     </>
   )
