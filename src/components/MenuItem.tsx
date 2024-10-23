d0e01be2dd0449f097a589fe4db3b37b45c5fde6
Samir Vergara Pastor <132174621+Samirvp15@users.noreply.github.com>
2024-10-22 21:50:30 -0500

    add MenuItem Component and data.js

---
+++ src/components/MenuItem.tsx
@@ -0,0 +1,22 @@
+import type { MenuItem } from "../types"
+
+
+
+type MenuItemProps = {
+    item: MenuItem
+}
+
+
+export default function MenuItem({item}: MenuItemProps) {
+  return (
+    <>
+    <button className=" border-2 border-amber-400 hover:bg-amber-200 w-full p-3 flex justify-between">
+    <p>{item.name}</p>
+    <p className=" font-black">${item.price}</p>
+
+    </button>
+    
+    </>
+  )
+}
+
